4ddb15ac14ee369e282fc486ba88a7bc0ecdbf18
Justin <justinmann.mail@gmail.com>
2024-07-09 12:50:27 -0600

    v1

---
+++ Api/MyResumeFunction.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using BlazorApp.Shared;
+using System.Text.Json;
+using System.Net;
+
+namespace Api
+{
+    public class HttpTrigger
+    {
+        private readonly ILogger _logger;
+
+        public HttpTrigger(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<HttpTrigger>();
+        }
+
+        [Function("MyResume")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "myResume")] HttpRequestData req)
+        {
+            var resumeResponse = JsonFileReader.Read<DigitalResumeModel>(@".\StaticData\Resumes\JustinMann_062024.json");
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(resumeResponse);
+            return response;
+        }
+
+        public static class JsonFileReader
+        {
+            public static T Read<T>(string filePath)
+            {
+                using FileStream stream = File.OpenRead(filePath);
+                return JsonSerializer.Deserialize<T>(stream);
+            }
+        }
+    }
+}
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorApp.Client;
+using Blazorise;
+using Blazorise.Bootstrap5;
+using Blazorise.Icons.FontAwesome;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,4 +11,15 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
 
+builder.Services
+    .AddBlazorise(options =>
+    {
+        options.Immediate = true;
+    })
+    .AddBootstrap5Providers()
+    .AddFontAwesomeIcons();
+
 await builder.Build().RunAsync();
+
+
+
