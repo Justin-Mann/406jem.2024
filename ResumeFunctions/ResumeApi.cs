2600d9ff77a6bd22b81a6342220faefa1aba37da
Justin E. Mann <Justin-Mann@users.noreply.github.com>
2025-04-11 08:04:10 -0600

    try another azure function from scratch

---
+++ ResumeFunctions/ResumeApi.cs
@@ -0,0 +1,37 @@
+using Azure;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using ResumeFunctions.Models;
+using System.Net;
+using System.Text.Json;
+
+namespace ResumeFunctions
+{
+    public class ResumeApi
+    {
+        private readonly ILogger<ResumeApi> _logger;
+
+        public ResumeApi(ILogger<ResumeApi> logger)
+        {
+            _logger = logger;
+        }
+
+        [Function("resumes")]
+        public IActionResult GetAllResumes([HttpTrigger(AuthorizationLevel.Function, "get")] HttpRequest req)
+        {
+            var resumeJsonResponse = JsonFileReader.Read<DigitalResumeModel[]>(@".\StaticData\Resumes\JustinMann_062024.json");
+            return new OkObjectResult(resumeJsonResponse ?? null);
+        }
+
+        internal static class JsonFileReader
+        {
+            public static T Read<T>(string filePath)
+            {
+                using FileStream stream = File.OpenRead(filePath);
+                return JsonSerializer.Deserialize<T>(stream);
+            }
+        }
+    }
+}
